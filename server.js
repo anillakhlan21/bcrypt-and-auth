788a57ea38b3e839b8acc67bf6f3d671e1748a6c
anil_kumar <anil.kumar@altorumleren.com>
2022-04-25 07:57:22 +0530

    project completed

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bcrypt =  require('bcrypt');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+app.get('/users',(req,res)=>{
+    res.json(users);
+})
+
+app.post('/users',async (req,res)=>{
+    try{
+        // const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password,10 );
+        // console.log(salt);
+        // console.log(hashedPassword);
+        const user = {name : req.body.name, password : hashedPassword}
+        users.push(user);
+        res.status(201).send();
+    } catch{
+        res.status(500).send();
+    }
+})
+
+app.post('/users/login', async(req,res)=>{
+    const user = users.find(user => user.name = req.body.name);
+    if(user ==null){
+        res.status(400).send("Cannot find user");
+    }
+    try{
+        if(await bcrypt.compare(req.body.password, user.password)){
+            res.send("Success");
+        }else{
+            res.send("password incorrect");
+        }
+    }catch{
+        return res.status(500).send();
+    }
+})
+
+
+app.listen(3000,(err)=>{
+    if(err) console.log(err);
+    console.log("App is runnning on port 3000");
+})
